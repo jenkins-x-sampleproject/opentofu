0f78f336b26204e80e095cb5d3a179ff3266edb2
kirishikak98 <134593698+kirishikak98@users.noreply.github.com>
2024-12-16 12:26:31 +0530

    Update context_apply.go
    Signed-off-by: kirishikak98 <134593698+kirishikak98@users.noreply.github.com>

--- internal/tofu/context_apply.go
+++ internal/tofu/context_apply.go
@@ -37,6 +37,7 @@ func (c *Context) Apply(plan *plans.Plan, config *configs.Config) (*states.State
 
 	var diags tfdiags.Diagnostics
 	for _, rc := range plan.Changes.Resources {
+		// Handle import hooks
 		if rc.Importing != nil {
 			for _, h := range c.hooks {
 				if hookDiags := handleImportHooks(h, rc.Addr, rc.Importing); hookDiags.HasErrors() {
@@ -45,13 +46,14 @@ func (c *Context) Apply(plan *plans.Plan, config *configs.Config) (*states.State
 			}
 		}
 
+		// Skipping forget hooks as `PreApplyForget` and `PostApplyForget` are undefined
 		if rc.Action == plans.Forget {
 			log.Printf("[DEBUG] Forget action detected for resource: %s", rc.Addr)
-			// Skipping PreApplyForget/PostApplyForget as these methods are not defined in Hook.
 		}
 	}
 
-	ctx := context.Background() // Create a standard context
+	// Apply graph changes
+	ctx := context.Background()
 	graph, operation, graphDiags := c.applyGraph(plan, config, true)
 	diags = diags.Append(graphDiags)
 	if diags.HasErrors() {
@@ -94,86 +96,8 @@ Note that the -target option is not suitable for routine use, and is provided on
 	return newState, diags
 }
 
-func (c *Context) applyGraph(plan *plans.Plan, config *configs.Config, validate bool) (*Graph, walkOperation, tfdiags.Diagnostics) {
-	var diags tfdiags.Diagnostics
-
-	variables := InputValues{}
-	for name, dyVal := range plan.VariableValues {
-		val, err := dyVal.Decode(cty.DynamicPseudoType)
-		if err != nil {
-			diags = diags.Append(tfdiags.Sourceless(
-				tfdiags.Error,
-				"Invalid variable value in plan",
-				fmt.Sprintf("Invalid value for variable %q recorded in plan file: %s.", name, err),
-			))
-			continue
-		}
-
-		variables[name] = &InputValue{
-			Value:      val,
-			SourceType: ValueFromPlan,
-		}
-	}
-	if diags.HasErrors() {
-		return nil, walkApply, diags
-	}
-
-	for name := range config.Module.Variables {
-		if _, ok := variables[name]; ok {
-			continue
-		}
-		variables[name] = &InputValue{
-			Value:      cty.NilVal,
-			SourceType: ValueFromPlan,
-		}
-	}
-
-	operation := walkApply
-	if plan.UIMode == plans.DestroyMode {
-		operation = walkDestroy
-	}
-
-	graph, moreDiags := (&ApplyGraphBuilder{
-		Config:             config,
-		Changes:            plan.Changes,
-		State:              plan.PriorState,
-		RootVariableValues: variables,
-		Plugins:            c.plugins,
-		Targets:            plan.TargetAddrs,
-		ForceReplace:       plan.ForceReplaceAddrs,
-		Operation:          operation,
-		ExternalReferences: plan.ExternalReferences,
-	}).Build(addrs.RootModuleInstance)
-	diags = diags.Append(moreDiags)
-	if moreDiags.HasErrors() {
-		return nil, walkApply, diags
-	}
-
-	return graph, operation, diags
-}
-
-// ApplyGraphForUI is a last vestige of graphs in the public interface of
-// Context (as opposed to graphs as an implementation detail) intended only for
-// use by the "tofu graph" command when asked to render an apply-time
-// graph.
-//
-// The result of this is intended only for rendering ot the user as a dot
-// graph, and so may change in future in order to make the result more useful
-// in that context, even if drifts away from the physical graph that OpenTofu
-// Core currently uses as an implementation detail of planning.
-func (c *Context) ApplyGraphForUI(plan *plans.Plan, config *configs.Config) (*Graph, tfdiags.Diagnostics) {
-	// For now though, this really is just the internal graph, confusing
-	// implementation details and all.
-
-	var diags tfdiags.Diagnostics
-
-	graph, _, moreDiags := c.applyGraph(plan, config, false)
-	diags = diags.Append(moreDiags)
-	return graph, diags
-}
-
 // handleImportHooks manages the hooks for the Importing operation.
-func handleImportHooks(h Hook, addr addrs.AbsResourceInstance, importing plans.Importing) tfdiags.Diagnostics {
+func handleImportHooks(h Hook, addr addrs.AbsResourceInstance, importing *plans.ImportingSrc) tfdiags.Diagnostics {
 	var diags tfdiags.Diagnostics
 
 	if _, err := h.PreApplyImport(addr, importing); err != nil {
@@ -194,27 +118,3 @@ func handleImportHooks(h Hook, addr addrs.AbsResourceInstance, importing plans.I
 
 	return diags
 }
-
-// handleForgetHooks manages the hooks for the Forget operation.
-func handleForgetHooks(h Hook, addr addrs.AbsResourceInstance) tfdiags.Diagnostics {
-	var diags tfdiags.Diagnostics
-
-	if _, err := h.PreApplyForget(addr); err != nil {
-		diags = diags.Append(tfdiags.Sourceless(
-			tfdiags.Error,
-			"PreApplyForget hook failed",
-			err.Error(),
-		))
-	}
-
-	if _, err := h.PostApplyForget(addr); err != nil {
-		diags = diags.Append(tfdiags.Sourceless(
-			tfdiags.Error,
-			"PostApplyForget hook failed",
-			err.Error(),
-		))
-	}
-
-	return diags
-}
-
